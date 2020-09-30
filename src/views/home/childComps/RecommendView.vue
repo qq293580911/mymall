1674dfedcaa79f3cc52a51db2f1d3ac03b2cfc18
qq293580911 <qq293580911@qq.com>
2020-09-30 10:59:55 +0800

    'BS的基本使用'

---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="recommend">
+    <div v-for="(item,index) in recommends" :key="index" class="recommend-item">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+      </a>
+      <div>{{item.title}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'RecommendView',
+  props:{
+    recommends:{
+      type:Array,
+      default(){
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.recommend{
+  display: flex;
+  width: 100%;
+  text-align: center;
+  font-size: 12px;
+  padding: 5px 0px;
+  border-bottom: 8px #eeeeee solid;
+}
+
+.recommend-item{
+  flex: 1;
+}
+
+.recommend-item img{
+  width: 70px;
+  height: 70px;
+  margin-bottom: 10px;
+}
+</style>
